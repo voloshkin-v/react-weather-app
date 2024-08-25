cb482fc84214f7317a367639d610b3c9a503837c
Vitalii Voloshyn <voloshin.v.02@gmail.com>
2024-08-25 14:18:46 +0200

    update prettier and extend tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: '#030616',
+        foreground: '#f2f5f9',
+
+        secondary: {
+          DEFAULT: '#20293a',
+          foreground: '#97a3b6',
+        },
+      },
+      borderRadius: {
+        DEFAULT: '1.25rem',
+      },
+    },
   },
   plugins: [],
 };
