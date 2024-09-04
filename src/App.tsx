0f17dc5d1919e9098987c480d7bd02cf9ad9d45c
Vitalii Voloshyn <voloshin.v.02@gmail.com>
2024-09-04 21:06:21 +0200

    add ui for idle state

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { useTranslation } from 'react-i18next';
+
 import Header from '@/components/Header/Header';
 import WeatherView from '@/components/Weather/WeatherView';
 import Error from '@/components/Weather/Error';
@@ -7,13 +9,19 @@ import { useWeather } from '@/context/Weather';
 
 const App = () => {
   const { status } = useWeather();
+  const { t } = useTranslation();
 
   return (
     <div className="flex min-h-svh flex-col">
       <Header />
 
       <main className="container flex-1 p-4">
-        {status === 'idle' && <p>Intro</p>}
+        {status === 'idle' && (
+          <div className="text-center">
+            <p>{t('common.cta')}</p>
+          </div>
+        )}
+
         {status === 'pending' && <Spinner />}
         {status === 'error' && <Error />}
         {status === 'success' && <WeatherView />}
